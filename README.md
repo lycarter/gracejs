8104ccef2aebeb65448d43a02422e6c84acd428e
Landon Y Carter <lcarter@mit.edu>
2015-02-25 17:56:23 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# GraceJS
+
+I decided to make a good friend of mine a js library, in the style of victorhh.com/antoniojs
+
+See the result at http://lcarter.mit.edu/gracejs
---
+++ grace.js
@@ -0,0 +1,58 @@
+/*!
+ * Grace.js
+ * http://lcarter.mit.edu/gracejs
+ *
+ * Author: Landon Carter (with significant influence/copying from Victor Hung's antoniojs script)
+ * Released under the MIT license
+ *
+ * Date: 2015-01-01
+ */
+ 
+// Pardon me, I've never coded pure Javascript
+// This will be interesting.
+
+var GRACE = "1031149799101";
+var BIRTHDAY = 1/1;
+
+var img = document.createElement('img');
+var logger = [];
+var position = -2000;
+var graceGo;
+var activateBtn = document.getElementById('activate');
+
+img.src = "http://lcarter.mit.edu/gracejs/thegal.png";
+img.style.height = "100%";
+img.style.position = "fixed";
+img.style.left = position + "px";
+img.style.bottom = "0";
+img.style.zIndex = 2015;
+
+document.addEventListener('keypress', function (e) {
+  console.log('sup');
+  console.log(e.which)
+  logger += '' + e.which;
+  if (GRACE.indexOf(logger) != 0) {
+    logger = "";
+  } else if (GRACE === logger){
+    console.log('Go, Grace!');
+    graceGo = setInterval(moveTheGal, 24);
+  }
+})
+
+if (activateBtn) {
+  activateBtn.addEventListener('click', function() {
+    console.log('Go, Grace!');
+    graceGo = setInterval(moveTheGal, 24);
+  })
+}
+
+var moveTheGal = function() {
+  if (position < -1) {
+    position *= BIRTHDAY/2;
+  } else {
+    clearInterval(graceGo);
+  }
+  img.style.left = position + "px";
+}
+
+document.getElementsByTagName('html')[0].appendChild(img);
